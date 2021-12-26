94bd09f1c47848a9280b235873c5960f29b70327
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-12-26 18:02:04 +0530

    Create GlobalStyles.js

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: #031c22;
+    color: #FFFFFF;
+    font-family: 'Tinos';
+    transition: all 0.50s linear;
+    margin: 0;
+    padding: 0;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    list-style: none;
+  }
+  a {
+    color: #FFFFFF;
+    text-decoration: none;
+  }
+  a:hover {
+    color: #91f3fc;
+  }
+  
+  input[type="text"] {
+    color: #FFFFFF;
+  }
+`;
